cb0dd5410d96248fbda55129a5ad88b5284fe70c
li yue hong <yhli394@163.com>
2022-03-05 22:29:54 +0800

    编写docker-compose.yml文件，在蒋师兄的帮助下弄懂了docker中volumes的原理

--- src/main/java/com/liyuehong/weeklyreport/model/User.java
+++ src/main/java/com/liyuehong/weeklyreport/model/User.java
@@ -61,8 +61,13 @@ public class User implements UserDetails{
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
         User user = (User) o;
         return Objects.equals(username, user.username);
     }
